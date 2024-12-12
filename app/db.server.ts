b95cb1c836428d57c13dedc93b60bd33d1cc1184
Akasha-freedom <akasha@freedommedia.com>
2024-12-12 23:07:57 +0500

    Your commit message here

--- app/db.server.ts
+++ app/db.server.ts
@@ -4,31 +4,12 @@ declare global {
   var prisma: PrismaClient;
 }
 
-// Create or reuse a Prisma Client instance
+const prisma: PrismaClient = global.prisma || new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
-    global.prisma
-      .$connect()
-      .then(() => console.log("Connected to the database"))
-      .catch((err) => {
-        console.error("Error connecting to the database:", err);
-        process.exit(1); // Exit process if the connection fails
-      });
   }
-}
-
-const prisma: PrismaClient = global.prisma || new PrismaClient();
-
-// Ensure database connection in production mode
-if (process.env.NODE_ENV === "production") {
-  prisma
-    .$connect()
-    .then(() => console.log("Connected to the database"))
-    .catch((err) => {
-      console.error("Error connecting to the database:", err);
-      process.exit(1); // Exit process if the connection fails
-    });
 }
 
 export default prisma;
