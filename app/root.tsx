9155f890f92612710919b1270b18d0c5f8939330
Akasha-freedom <akasha@freedommedia.com>
2025-02-10 23:49:38 +0500

    changes done

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ export function loader() {
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME ?? "",
     CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET ?? "",
   };
-  console.log('yeah hy',ENV);
+  console.log('yeah h',ENV);
   return new Response(JSON.stringify({ ENV }), {
     headers: { "Content-Type": "application/json" },
   });
